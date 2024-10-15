c8125211237cc61c77c7ba7aedb36351b1e42570
Hannah Reed <hannahfrancesreed@gmail.com>
2024-10-15 10:24:22 +0100

    removied comment text

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -85,23 +85,3 @@ describe(" /api/articles/:article_id", () => {
       });
   });
 });
-
-// Description
-// Should:
-
-// be available on /api/articles/:article_id.
-// get an article by its id.
-// Responds with:
-
-// an article object, which should have the following properties:
-// author
-// title
-// article_id
-// body
-// topic
-// created_at
-// votes
-// article_img_url
-// Consider what errors could occur with this endpoint, and make sure to test for them.
-
-// Remember to add a description of this endpoint to your /api endpoint.
