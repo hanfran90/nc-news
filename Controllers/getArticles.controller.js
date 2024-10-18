e83d69959c4f2a59186af2b7fcbc713d7bea6e56
Hannah Reed <hannahfrancesreed@gmail.com>
2024-10-18 09:40:54 +0100

    ticket 12 - articles by topic query

--- Controllers/getArticles.controller.js
+++ Controllers/getArticles.controller.js
@@ -1,10 +1,14 @@
 const fetchArticles = require("../Models/fetchArticles.models");
 
 function getArticles(request, response, next) {
-  const { sort_by, order } = request.query;
+  const { sort_by, order, topic } = request.query;
 
-  fetchArticles(sort_by, order)
+  fetchArticles(sort_by, order, topic)
     .then((articles) => {
+      if (articles.length === 0) {
+        response.status(404).send({ msg: "No articles found for that topic!" });
+      }
+
       response.status(200).send({ articles });
     })
     .catch((err) => {
